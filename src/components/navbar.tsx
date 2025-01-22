d5d390a8078db31d9b1106e861f1aae5af532204
unknown <ngoumkwa.kenne@gmail.com>
2025-01-22 20:29:27 +0100

    mise à jour Navnar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,11 +17,12 @@ export const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-background/80 mx-10 backdrop-blur-lg border-b border-border">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-background/80 mx-2 md:mx-10 backdrop-blur-lg border-b border-border">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <Link to="/" className="text-2xl font-bold gradient-text">
-            Portfolio
+          {/* <Link to="/" className="text-2xl font-bold gradient-text"> */}
+          <Link to="/">
+            <img src="/images/kabrel.png" className="h-28 w-auto" />
           </Link>
 
           {/* Desktop Menu */}
